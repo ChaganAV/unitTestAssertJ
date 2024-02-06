320ade8089456d48e19cc7fdfa881b7a4d7853f7
Alexandr <jwchaganav@gmail.com>
2024-02-06 20:54:59 +0400

    calculator

---
+++ src/main/java/model/Calculator.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Calculator {
+    private double summa;
+    private double discont;
+
+    public Calculator() {
+    }
+    public double calculateDiscount(){
+
+    }
+}
