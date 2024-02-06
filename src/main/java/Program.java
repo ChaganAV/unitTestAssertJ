e906a4b5fadd0da9b29c71fe42742ea489cc0ddf
Alexandr <jwchaganav@gmail.com>
2024-02-06 21:16:45 +0400

    assert

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -4,5 +4,6 @@ public class Program {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         assert 5 == calculator.calculateDiscount(25,5);
+        System.out.printf("true");
     }
 }
