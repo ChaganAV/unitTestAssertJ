9b0c0a51e2e4c4714854c876f216f049f98b1636
Alexandr <jwchaganav@gmail.com>
2024-02-07 08:43:20 +0400

    test

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -3,7 +3,21 @@ import model.Calculator;
 public class Program {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        assert 5 == calculator.calculateDiscount(25,5);
-        System.out.printf("true");
+
+        try {
+            double sum = 25;
+            int disc = 5;
+            assert 5 == calculator.calculateDiscount(sum, disc);
+            System.out.printf("Стоимость %.2f, скидка %d, ваша сумма со скидкой составляет %.2f",
+                    sum, disc, calculator.calculateDiscount(sum, disc));
+            System.out.println();
+            sum = 25;
+            disc = 105;
+            assert 5 == calculator.calculateDiscount(sum, disc);
+            System.out.printf("Стоимость %.2f, скидка %d, ваша сумма со скидкой составляет %.2f",
+                    sum, disc, calculator.calculateDiscount(sum, disc));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
