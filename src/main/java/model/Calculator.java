856d2cf7d94dcb493a5c0e93f45c88481d84019f
Alexandr <jwchaganav@gmail.com>
2024-02-07 09:10:00 +0400

    isPositive

--- src/main/java/model/Calculator.java
+++ src/main/java/model/Calculator.java
@@ -10,12 +10,10 @@ public class Calculator {
     public double calculateDiscount(double sum, int discont ){
         double resultDiscont = sum * discont / 100;
         double result = sum - resultDiscont;
-        //assertThat(result).isNegative().isInstanceOf(ArithmeticException.class);
         assertThat(result).isPositive();
+        //assertThat(result).isPositive().isInstanceOf(ArithmeticException.class);
 
-        /*if(result< 0) {
-            throw new ArithmeticException("Скидка не может быть больше суммы");
-        }*/
+        //if(assertThat(result).isNegative()) throw new ArithmeticException("Скидка не может быть больше суммы");
         return result;
     }
 }
